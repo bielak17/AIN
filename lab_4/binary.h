1b2f1c5f4ad5af4c3355efefc4414bda9d8918c2
ffijalkowski <filipfijalkowski00@gmail.com>
2025-11-12 20:52:25 +0100

    Delete lab_4 directory

--- lab_4/binary.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "functions.h"
-
-#include <random>
-#include <algorithm>
-
-RunResult run_single_real(int bits_per_dim, std::mt19937 &rng, int func_id);
--- lab_4/gray.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "functions.h"
-
-#include <random>
-#include <algorithm>
-
-RunResult run_single_gray(int, std::mt19937&, int );
