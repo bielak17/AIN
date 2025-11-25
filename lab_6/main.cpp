e59e21acecc28dcd57793d39785e048e3490eaee
ffijalkowski <filipfijalkowski00@gmail.com>
2025-11-25 12:33:53 +0100

    Update main.cpp

--- lab_6/main.cpp
+++ lab_6/main.cpp
@@ -22,43 +22,43 @@ int main()
     std::random_device rd;
     std::vector<std::vector<double>> all_runs(REPEATS, std::vector<double>(MAX_F_CALLS));
     // // Running experiments for Gray encoding
-    for (int func_id = 1; func_id <= 3; ++func_id)
-    {
-        for (auto current_dim : N)
-        {    
-            for (int run = 0; run < REPEATS; ++run) 
-            {
-                std::mt19937 run_rng(rd() + run * 7919 + current_dim * 101);
-                RunResult rr = run_single_gray(BITS_PER_DIM, run_rng, func_id, current_dim);
-                all_runs[run] = std::move(rr.running_best);
-            }
+    // for (int func_id = 1; func_id <= 3; ++func_id)
+    // {
+    //     for (auto current_dim : N)
+    //     {    
+    //         for (int run = 0; run < REPEATS; ++run) 
+    //         {
+    //             std::mt19937 run_rng(rd() + run * 7919 + current_dim * 101);
+    //             RunResult rr = run_single_gray(BITS_PER_DIM, run_rng, func_id, current_dim);
+    //             all_runs[run] = std::move(rr.running_best);
+    //         }
         
 
-            // Saving results to CSV
-            std::ostringstream fname;
-            fname << "results_gray_func_" << func_id << "dims_" << current_dim << ".csv";
-            std::ofstream of(fname.str());
+    //         // Saving results to CSV
+    //         std::ostringstream fname;
+    //         fname << "results_gray_func_" << func_id << "dims_" << current_dim << ".csv";
+    //         std::ofstream of(fname.str());
 
-            for (int r = 0; r < REPEATS; ++r) 
-            {
-                of << "run_" << (r + 1);
-                if (r < REPEATS - 1) of << ";";
-            }
-            of << "\n";
+    //         for (int r = 0; r < REPEATS; ++r) 
+    //         {
+    //             of << "run_" << (r + 1);
+    //             if (r < REPEATS - 1) of << ";";
+    //         }
+    //         of << "\n";
 
-            for (int step = 0; step < MAX_F_CALLS * current_dim; ++step) 
-            {
-                for (int r = 0; r < REPEATS; ++r) 
-                {
-                    of << all_runs[r][step];
-                    if (r < REPEATS - 1) of << ";";
-                }
-                of << "\n";
-            }
-            of.close();
-            std::cout << fname.str() << " file created." << std::endl;
-        }
-    }
+    //         for (int step = 0; step < MAX_F_CALLS * current_dim; ++step) 
+    //         {
+    //             for (int r = 0; r < REPEATS; ++r) 
+    //             {
+    //                 of << all_runs[r][step];
+    //                 if (r < REPEATS - 1) of << ";";
+    //             }
+    //             of << "\n";
+    //         }
+    //         of.close();
+    //         std::cout << fname.str() << " file created." << std::endl;
+    //     }
+    // }
 
     // Running experiments for Real encoding
     for (int func_id = 1; func_id <= 3; ++func_id)
@@ -104,3 +104,4 @@ int main()
     std::cout << "All four experiments completed. CSV files saved in current folder.\n";
     return 0;
 }
+
